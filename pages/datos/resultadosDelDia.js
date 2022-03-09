4a7ca1e7292e9c9169cc6e1be6192f3e4e9a2571
geyler <geylerps@gmail.com>
2022-03-09 18:40:13 -0300

    datos del dia

---
+++ pages/datos/resultadosDelDia.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+export default async function controller() {
+  let { data } = await axios.get("http://www.beisbolcubano.cu/");
+  let $ = cheerio.load(data);
+  let contenedor = $("#MainContent_MiniDayScore_UC_sidebarDayScore_Left");
+  let resultados = [];
+
+  $(contenedor)
+    .find(".Mini_DayScore")
+    .each(function () {
+      let estado = $(this).find(".Mini_DayScore_Row span").text();
+      resultados.push({
+        estado
+      });
+    });
+
+  return resultados;
+}
