abb52eb0d104fae251f8a1b14bfdd96db28134de
geyler <geylerps@gmail.com>
2022-03-10 13:33:21 -0300

    resultados del dia

--- datos/resultadosDelDia.js
+++ datos/resultadosDelDia.js
@@ -5,14 +5,48 @@ export default async function controller() {
   let { data } = await axios.get("http://www.beisbolcubano.cu/");
   let $ = cheerio.load(data);
   let contenedor = $("#MainContent_MiniDayScore_UC_sidebarDayScore_Left");
+  let contenedorAyer = $("#MainContent_PreviousMiniDayScore_UC_sidebarDayScore_Left");
   let resultados = [];
 
   $(contenedor)
     .find(".Mini_DayScore")
     .each(function () {
       let estado = $(this).find(".Mini_DayScore_Row span").text();
+
+      let visitante = $(this).find('div:nth-child(1) > span.Mini_DayScore_Siglas.MDSN').text();
+      let visitanteLogo = $(this).find('div:nth-child(1) > img').attr('src');
+      let visitanteCarreras = $(this).find('div:nth-child(2) > div:nth-child(1) > span').text();
+
+      let local = $(this).find('div:nth-child(2) > span.Mini_DayScore_Siglas.MDSN').text();
+      let localLogo = $(this).find('div:nth-child(2) > img').attr('src');
+      let localCarreras  = $(this).find('div:nth-child(2) > div:nth-child(2) > span').text();
+
+      let vlogo = 'http://www.beisbolcubano.cu/' + visitanteLogo;
+      let llogo = 'http://www.beisbolcubano.cu/' + localLogo;
+
       resultados.push({
-        estado
+        estado, visitante, local, vlogo, llogo, visitanteCarreras, localCarreras
+      });
+    });
+
+    $(contenedorAyer)
+    .find(".Mini_DayScore")
+    .each(function () {
+      let estadoAyer = $(this).find(".Mini_DayScore_Row span").text();
+
+      let visitanteAyer = $(this).find('div:nth-child(1) > span.Mini_DayScore_Siglas.MDSN').text();
+      let visitanteLogoAyer = $(this).find('div:nth-child(1) > img').attr('src');
+      let visitanteCarrerasAyer = $(this).find('div:nth-child(2) > div:nth-child(1) > span').text();
+
+      let localAyer = $(this).find('div:nth-child(2) > span.Mini_DayScore_Siglas.MDSN').text();
+      let localLogoAyer = $(this).find('div:nth-child(2) > img').attr('src');
+      let localCarrerasAyer = $(this).find('div:nth-child(2) > div:nth-child(2) > span').text();
+
+      let vlogoAyer = 'http://www.beisbolcubano.cu/' + visitanteLogoAyer;
+      let llogoAyer = 'http://www.beisbolcubano.cu/' + localLogoAyer;
+
+      resultados.push({
+        estadoAyer, visitanteAyer, localAyer, vlogoAyer, llogoAyer, visitanteCarrerasAyer, localCarrerasAyer
       });
     });
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,59 @@
-import resultadosDelDia from '../datos/resultadosDelDia';
+import resultadosDelDia from "../datos/resultadosDelDia";
 
 export default function Resultados(props) {
   return (
-    <ul>
-      {props?.data?.map((resultado, key) => (
-        <li key={key}>{resultado?.estado}</li>
-      ))}
-    </ul>
+    <div className="home">
+
+      <ul className="resultados-del-dia">
+        {props?.data?.map((resultado, key) => (
+           resultado.estado ? (<li key={key}>
+            <div className="estado-del-juego">{resultado?.estado}</div>
+            <div className="equipo-visitante resultado">
+              <div className="logo-equipo">
+                <img src={resultado?.vlogo} />
+                <span>{resultado?.visitante}</span>
+              </div>
+              <div className="carreras">{resultado?.visitanteCarreras}</div>
+            </div>
+            <div className="equipo-local resultado">
+              <div className="logo-equipo">
+                <img src={resultado?.llogo} />
+                <span>{resultado?.local}</span>
+              </div>
+              <div className="carreras">{resultado?.localCarreras}</div>
+            </div>
+          </li>) : ''
+        ))}
+      </ul>
+
+      <ul className="resultados-del-dia">
+        {props?.data?.map((resultado, key) => (
+         
+         
+         resultado.estadoAyer ? (<li key={key}>
+            <div className="estado-del-juego">{resultado?.estadoAyer}</div>
+            
+            <div className="equipo-visitante resultado">
+              <div className="logo-equipo">
+                <img src={resultado?.vlogoAyer} />
+                <span>{resultado?.visitanteAyer}</span>
+              </div>
+              <div className="carreras">{resultado?.visitanteCarrerasAyer}</div>
+            </div>
+
+            <div className="equipo-local resultado">
+              <div className="logo-equipo">
+                <img src={resultado?.llogoAyer} />
+                <span>{resultado?.localAyer}</span>
+              </div>
+              <div className="carreras">{resultado?.localCarrerasAyer}</div>
+            </div>
+
+          </li>) : ''
+        ))}
+      </ul>
+
+    </div>
   );
 }
 
